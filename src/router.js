1700a0879e1ca0cd30cd37cb02462d478e197deb
bravint <bravint@gmail.com>
2022-02-17 19:45:43 +0000

    edit token gen

--- src/router.js
+++ src/router.js
@@ -9,27 +9,36 @@ const mockUser = {
     profile: {
         firstName: 'Chris',
         lastName: 'Wolstenholme',
-        age: 43
-    }
+        age: 43,
+    },
 };
 
-const secret = 'secret'
+const secret = 'secret';
 
 router.post('/login', (req, res) => {
-    const {username, password} = req.body
-    if (username !== mockUser.username || password !== mockUser.password) res.status(401).send(`Incorrect credentials`)
-    const payload = {username, password, iat: 1561358034}
-    const token = jwt.sign(payload, secret)
-    res.json(token)
+    const { username, password } = req.body;
+
+    if (username !== mockUser.username || password !== mockUser.password) {
+        return res.status(401).json(`Incorrect credentials`);
+    }
+
+    const payload = {
+        username,
+        password,
+    };
+
+    const token = jwt.sign(payload, secret);
+
+    res.json(token);
 });
 
 router.get('/profile', (req, res) => {
-    const token = (req.headers.authorization)
+    const token = req.headers.authorization;
     try {
-        jwt.verify(token, secret)
-        res.json(mockUser.profile)
-    } catch(error) {
-        res.send(error)
+        jwt.verify(token, secret);
+        res.json(mockUser.profile);
+    } catch (error) {
+        res.send(error);
     }
 });
 
